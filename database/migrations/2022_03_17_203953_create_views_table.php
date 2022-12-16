f3d828a09b1758fbfed3a236e5e255d0f603d19e
jmtdbg <jmtdbg@gmail.com>
2022-12-16 11:06:10 -0400

    fix:base projeto

--- database/migrations/2022_03_17_203953_create_views_table.php
+++ database/migrations/2022_03_17_203953_create_views_table.php
@@ -13,14 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('views', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->uuid('user_id');
+            $table->uuid('lesson_id');
+            $table->integer('qty')->default(1);
             $table->timestamps();
+
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users');
+
+            $table->foreign('lesson_id')
+                    ->references('id')
+                    ->on('lessons');
         });
     }
 
@@ -31,6 +37,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('views');
     }
 };
